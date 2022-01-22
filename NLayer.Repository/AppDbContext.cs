0b2bee5849bf57be203da14f9abb687ff8b51f28
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2022-01-22 22:35:42 +0300

    Created & Updated Dateler düzenlendi.

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -4,41 +4,84 @@ using System.Reflection;
 
 namespace NLayer.Repository
 {
-	public class AppDbContext : DbContext
-	{
-		public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityreference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            entityreference.CreatedDate = DateTime.Now;
+                            break;
+                        case EntityState.Modified:
+                            Entry(entityreference).Property(m=>m.CreatedDate).IsModified = false;
+                            entityreference.Updateddate = DateTime.Now;
+                            break;
+
+                    }
+                }
+            }
+           return base.SaveChangesAsync(cancellationToken);
+        }
+		public override int SaveChanges()
 		{
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityreference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            entityreference.CreatedDate = DateTime.Now;
+                            break;
+                        case EntityState.Modified:
+                            Entry(entityreference).Property(m => m.CreatedDate).IsModified = false;
+                            entityreference.Updateddate = DateTime.Now;
+                            break;
 
+                    }
+                }
+            }
+            return base.SaveChanges();
 		}
-		public DbSet<Category> Categories { get; set; }
-		public DbSet<Product> Products { get; set; }
-		public DbSet<ProductFeature> ProductFeatures { get; set; }
-
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
-			modelBuilder.Entity<ProductFeature>().HasData(
-					new ProductFeature
-					{
-						Color = "Red",
-						Height = 40,
-						Width = 80,
-						Id = 1,
-						ProductId = 1,
-					},
-					new ProductFeature
-					{
-						Color = "Blue",
-						Height = 50,
-						Width = 20,
-						Id = 2,
-						ProductId = 3,
-					}
-				);
+            modelBuilder.Entity<ProductFeature>().HasData(
+                    new ProductFeature
+                    {
+                        Color = "Red",
+                        Height = 40,
+                        Width = 80,
+                        Id = 1,
+                        ProductId = 1,
+                    },
+                    new ProductFeature
+                    {
+                        Color = "Blue",
+                        Height = 50,
+                        Width = 20,
+                        Id = 2,
+                        ProductId = 3,
+                    }
+                );
 
-			base.OnModelCreating(modelBuilder);
-		}
+            base.OnModelCreating(modelBuilder);
+        }
 
-	}
+    }
 }
